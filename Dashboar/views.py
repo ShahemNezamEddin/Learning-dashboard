c5f6dc4ea6f41b4582d975202350ed08b271c645
ShahemNezamEddin <shahem.nezam.eddin@gmail.com>
2023-05-02 16:42:01 +0000

    #5 Add notes template, view, url, model

--- Dashboar/views.py
+++ Dashboar/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
 # home views.
@@ -6,3 +7,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'dashboard/home.html')
+
+# home views.
+
+
+def notes(request):
+    notes = Notes.objects.filter(user=request.user)
+    context = {'notes': notes}
+    return render(request, 'dashboard/notes.html', context)
